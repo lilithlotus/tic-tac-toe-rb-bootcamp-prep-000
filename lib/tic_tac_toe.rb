7303c776c3b668f9894c139c5e628923df782867
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-20 23:02:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,3 +44,21 @@ def turn(board)
   end
   display_board(board)
 end
+
+def turn_count(board)
+  counter = 0
+  board.each do |plays|
+    if plays == "X" || plays == "O"
+      counter += 1
+    end
+  end
+ counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
