d3e8dcd13937b2fb64e6f038b1a16902bb99d088
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-20 23:00:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,3 +32,15 @@ def valid_move?(board, index)
     return false
   end
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  index = input_to_index(gets.strip)
+  if valid_move?(board, index)
+    move(board, index)
+  else
+    puts "Invalid input."
+    turn(board)
+  end
+  display_board(board)
+end
