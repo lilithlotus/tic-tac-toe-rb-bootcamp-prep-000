2edfc92360c72a855660b740a8bf6c2991d48285
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-20 22:59:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,3 +25,10 @@ def display_board(board)
   !(board[index].nil? || board[index] == " ")
 end
 
+def valid_move?(board, index)
+  if index.between?(0, 8)  && position_taken?(board, index) == false
+    return true
+  else
+    return false
+  end
+end
